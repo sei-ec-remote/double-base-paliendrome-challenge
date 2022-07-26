21487d2eb1a13b8797e20841aec20e2803327c79
nickesparza <nickesparza@gmail.com>
2022-07-26 08:24:16 -0500

    complete double base palindrome challenge

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -18,10 +18,20 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 This question is from - https://projecteuler.net/problem=36
 */
-const doubleBasePalindromeSum = function (/* base1, base2, upperLimit */) {
-
+const doubleBasePalindromeSum = function (base1, base2, upperLimit) {
+    let sum = 0
+    for (let i = 1; i <= upperLimit; i++) {
+        let binNum = i.toString(base1)
+        if (i.toString(base2) === String(i).split('').reverse().join('') && binNum === binNum.split('').reverse().join('')) {
+            // console.log(`${i} is a palindrome, and so is its binary ${binNum}`)
+            sum += i
+        }
+    }
+    console.log(sum)
+    return sum
 }
 
+doubleBasePalindromeSum(2, 10, 1000000)
 
 // DO NOT MODIFY
 module.exports = {
