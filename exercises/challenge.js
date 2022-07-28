6d8d4a5fc4177dbe14e1c77520fa08486231fd8f
shaialoni <gitShai2022>
2022-07-28 06:49:36 -0700

    added comments for thought process

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -19,20 +19,26 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 This question is from - https://projecteuler.net/problem=36
 */
 const doubleBasePalindromeSum = function (base1, base2, upperLimit) {
-      //first - I want to be able to convert a nmber from decimal to binary:
-      // To do that, we run:
+      // since we need to find ALL numbers that are palindrome on two bases, we need to run a loop that is going to iterate over all numbers, up to the upperLimit
       let sumOfAll = null
       for (let i = 0; i < upperLimit; i++) {
+            // we need to find a way to check if a number is a palindrome, so lets write a function for that
             const checkPalindrome = (number, base) => {
+                  // we take in two parameters, number and base
+                  // we turn the number into a string with a base based on input, and split the digits into an array of single digits
                   const arr = (number).toString(base).split('')
+                  // we take the same number, split it into the same array, and reverse the array.
                   const arrRev = (number).toString(base).split('').reverse()
+                  // we check to see if the arrays hold the same value, and return true r false
                   return JSON.stringify(arr) === JSON.stringify(arrRev)
             }
-            
+            // using the parameters that are being passed, we check the same number for palindrome in the two bases
+            // if BOTH are true, we add the sum of the number being tested to a sumOfAll variable
             if (checkPalindrome(i, base1) && checkPalindrome(i, base2) ) {
                   sumOfAll += i
             }
       }
+      // we get the function to return the variable
       return sumOfAll
       
 }
