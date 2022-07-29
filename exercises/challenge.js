2d3ef0f31a5a62eb570edb13966c65fbc0027aee
jamestylladams <jamestylladams@gmail.com>
2022-07-28 19:40:53 -0600

    way better

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -22,37 +22,22 @@ This question is from - https://projecteuler.net/problem=36
 const doubleBasePalindromeSum = function (base1, base2, upperLimit) {
   const fullArr = [...Array(upperLimit).keys()];
   fullArr.shift();
-
-  //get all numbers to limit that are palindromes in base two
+  
   let oneArray = fullArr.filter((i) => {
-    let newString = i.toString(base1);
-    let revString = parseInt(newString.split("").reverse().join(""));
-    newString = parseInt(newString);
-    if (newString === revString) {
+    let revString = i.toString(base1).split("").reverse().join("");
+    let revStringTwo = i.toString(base2).split("").reverse().join("");
+    if (i.toString(base1) === revString && i.toString(base2) === revStringTwo) {
       return i;
     }
   });
-
-  //use items from base 2 array that are also base 10 palindromes
-  let twoArray = oneArray.filter((i) => {
-    let newString = i.toString(base2);
-    let revString = parseInt(newString.split("").reverse().join(""));
-    newString = parseInt(newString);
-    if (newString === revString) {
-      return i;
-    }
-  });
-
+  
   //add everything up
-  let final = twoArray.reduce((tot, curr) => {
+  let final = oneArray.reduce((tot, curr) => {
     return tot + curr;
   });
 
   return final;
 };
-
-doubleBasePalindromeSum(2, 10, 500);
-
 // DO NOT MODIFY
 module.exports = {
   doubleBasePalindromeSum,
