121387798f7d1618dbe9f924484186f1f426edc9
eshalali <eshal.a.ali@gmail.com>
2022-07-29 08:18:01 -0500

    Finished diagnostic

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -8,6 +8,8 @@ TEST: To test run the commands `npm run test` in your terminal
       Don't worry about capitalization.
 */
 
+const { Base } = require("mocha/lib/reporters")
+
 /*
 Question 1
 The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
@@ -18,8 +20,25 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 This question is from - https://projecteuler.net/problem=36
 */
-const doubleBasePalindromeSum = function (/* base1, base2, upperLimit */) {
-
+const doubleBasePalindromeSum = function ( base1, base2, upperLim ) {
+      let sum = 0
+      // testing every number in base 1 up to upper limit
+      for (let i = 0; i < upperLim; i++) {
+            // must turn into string to reverse
+            let Base1 = i.toString(base1)
+            let reverseBase1 = Base1.split('').reverse().join('')
+            // if base 1 passes palindrome test
+            if (Base1 === reverseBase1) {
+                  // convert to base 2 and reverse
+                  let Base2 = i.toString(base2)
+                  let reverseBase2 = Base2.split('').reverse().join('')
+                  // if base 2 passes palindrome test, add i to sum
+                  if (Base2 === reverseBase2) {
+                        sum+=i
+                  }
+            }
+      }
+      return sum
 }
 
 
