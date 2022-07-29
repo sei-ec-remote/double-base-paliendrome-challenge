510f7e80a532d292833fa34bfe72c73fa206fba8
jamestylladams <jamestylladams@gmail.com>
2022-07-28 19:22:51 -0600

    shortening

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -22,48 +22,36 @@ This question is from - https://projecteuler.net/problem=36
 const doubleBasePalindromeSum = function (base1, base2, upperLimit) {
   const fullArr = [...Array(upperLimit).keys()];
   fullArr.shift();
-  //console.log(fullArr)
 
-  let oneArray = fullArr.map((i) => {
+  //get all numbers to limit that are palindromes in base two
+  let oneArray = fullArr.filter((i) => {
     let newString = i.toString(base1);
-    let revString = newString.split("").reverse().join("");
+    let revString = parseInt(newString.split("").reverse().join(""));
     newString = parseInt(newString);
-    revString = parseInt(revString);
     if (newString === revString) {
       return i;
     }
   });
-  //this array has palindromes in binary
-  oneArray = oneArray.filter((i) => {
-    if (i !== undefined) {
-      return i;
-    }
-  });
-  console.log(oneArray);
 
-  //you're almost there. next do the same thing with base 10 through the array you just got
-  let twoArray = oneArray.map((i) => {
+  //use items from base 2 array that are also base 10 palindromes
+  let twoArray = oneArray.filter((i) => {
     let newString = i.toString(base2);
-    let revString = newString.split("").reverse().join("");
+    let revString = parseInt(newString.split("").reverse().join(""));
     newString = parseInt(newString);
-    revString = parseInt(revString);
     if (newString === revString) {
-      return newString;
-    }
-  });
-  twoArray = twoArray.filter((i) => {
-    if (i !== undefined) {
       return i;
     }
   });
 
+  //add everything up
   let final = twoArray.reduce((tot, curr) => {
     return tot + curr;
   });
+
   return final;
 };
 
-doubleBasePalindromeSum(2, 10, 50);
+doubleBasePalindromeSum(2, 10, 500);
 
 // DO NOT MODIFY
 module.exports = {
