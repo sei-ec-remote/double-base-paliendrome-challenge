023554daf96e7d88e4f366e2dcb4192592fb7648
RandyD <rdsoundguy@gmai.com>
2022-07-29 09:21:20 -0400

    It works

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -18,9 +18,58 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 This question is from - https://projecteuler.net/problem=36
 */
-const doubleBasePalindromeSum = function (/* base1, base2, upperLimit */) {
+const doubleBasePalindromeSum = function (x,y,upperLimit) {
+
+
+      const checkPalindrome = (input) => {
+
+            let makeString = input.toString()
+            let holderString = ""
+
+            for (let i = makeString.length-1; i >=0; i--){
+                  holderString = holderString + makeString[i]
+            }
+
+            if (makeString === holderString) {
+                  return true
+            } else {return false}
+      }
+
+      let arrayOfNums = []
+      let sumTotal = 0
+
+
+
+      while (sumTotal < upperLimit){
+
+            for(let i = 0; i < upperLimit; i++){  
+                  if(checkPalindrome(i)){
+                        let binary = Number(i).toString(2)
+                        let strippedBinary = binary.replace(/^0+/, '')
+
+                        if(checkPalindrome(strippedBinary)){
+                              arrayOfNums.push(i)
+                        }
+                  }
+            sumTotal = arrayOfNums.reduce((partialSum, a) => partialSum + a, 0)
+            } 
+      }
+
+      return sumTotal
 
 }
+
+      
+      // convert to string
+      // check if pal
+      // if yes get base2      
+      // remove leading zeros  ??
+      // convert to string
+      // check binary nature
+      //  if palandrome, add decimal to a running total
+      // if total is less than 1mil keep going   
+
+
 
 
 // DO NOT MODIFY
